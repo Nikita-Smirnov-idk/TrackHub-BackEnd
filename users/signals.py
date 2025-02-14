368f8fd12dff6a1a033869f25ec0dc780bc2abe1
Nikita-Smirnov-idk <nikita.d.smirnov@gmail.com>
2025-02-15 01:17:41 +0300

    deleted print

--- users/signals.py
+++ users/signals.py
@@ -10,7 +10,6 @@ def create_superuser(sender, **kwargs):
         User = get_user_model()
         email=os.environ.get('DJANGO_SUPERUSER_EMAIL', 'sc@example.com')
         password=os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'asdASDASD123')
-        print(email, password)
         if not User.objects.filter(email=email).exists():
             User.objects.create_superuser(
                 email=email,
